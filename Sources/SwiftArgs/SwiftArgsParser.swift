64e955d08d1c35c2fd3280c214acf4a592a0cce7
Ardalan Samimi <ardalan.samimi@gmail.com>
2018-06-10 21:15:14 +0200

    Implemented parse(flagOption:)

--- Sources/SwiftArgs/SwiftArgsParser.swift
+++ Sources/SwiftArgs/SwiftArgsParser.swift
@@ -2,6 +2,7 @@
 // SwiftArgsParser.swift
 // Created by Ardalan Samimi on 2018-06-09
 //
+
 internal class SwiftArgsParser {
 
 	let validArguments: [Argument]
@@ -10,7 +11,7 @@ internal class SwiftArgsParser {
 	private var currentIndex: Int = 0
 
 	private var nextArgument: String? {
-		guard self.givenArguments.count < self.currentIndex else { return nil }
+		guard self.givenArguments.count > self.currentIndex else { return nil }
 		let currentArgument = self.givenArguments[currentIndex]
 		self.currentIndex += 1
 
@@ -21,8 +22,8 @@ internal class SwiftArgsParser {
 		self.validArguments = arguments
 	}
 
-	func start() {
-
+	func start(_ arguments: [String]? = CommandLine.arguments) {
+		self.givenArguments = arguments!
 		while let argument = self.nextArgument {
 			self.parse(argument)
 		}
@@ -33,20 +34,19 @@ internal class SwiftArgsParser {
 		guard let argument = self.validArguments[argument] else { return } // TODO: Throw error
 
 		if argument.type == .FlagOption {
-			self.parse(flagOption: argument as! FlagOption<Any>)
+			self.parse(flagOption: argument)
 		} else if argument.type == .SwitchOption {
-			self.parse(switchOption: argument as! SwitchOption)
+			self.parse(switchOption: argument)
 		} else if argument.type == .CommandOption {
 			self.parse(commandOption: argument as! CommandOption<Any>)
 		}
 	}
 
-	private func parse(switchOption: SwitchOption) {
-		switchOption.value = true
+	private func parse(switchOption: Argument) {
+		switchOption.setValue(true)
 	}
 
 	private func parse(commandOption: CommandOption<Any>) {
-
 		while let argument = self.nextArgument {
 			guard commandOption.takesArgument(argument) else {
 				self.currentIndex -= 1
@@ -60,44 +60,13 @@ internal class SwiftArgsParser {
 
 	}
 
-	private func parse(flagOption: FlagOption<Any>) {
+	private func parse(flagOption: Argument) {
+		guard let argument = self.nextArgument else {
+			// TODO: Throw error
+			return
+		}
 
+		flagOption.setValue(argument)
 	}
 
 }
-
-//
-// extension SwiftArgsParser {
-// 	func run(_ arguments: [String]? = nil) {
-// 		self.givenArguments = (arguments == nil) ? Array(CommandLine.arguments[1...]) : arguments!
-//
-// 		while (self.givenArguments.count > 0) {
-// 			guard let givenArgument = self.givenArguments.first else { return }
-// 			guard let validArgument = self.validArguments[argument] else { return } // TODO: Throw error
-//
-// 			self.givenArguments = Array(self.givenArguments[1...])
-//
-// 			switch validArgument.type {
-// 			case .Command:
-// 				self.parseCommand(validArgument)
-// 			case .Option:
-// 				self.parseOption(validArgument)
-// 				break
-// 			default:
-// 				break
-// 			}
-//
-// 		}
-//
-// 	}
-//
-// 	private func parseCommand(_ command: Command) {
-// 		guard let givenArgument = self.givenArguments.first else { return }
-// 		guard let validArgument = command.subCommands[]
-// 	}
-//
-// 	private func parseOption(_ name: String) {
-//
-// 	}
-//
-// }
