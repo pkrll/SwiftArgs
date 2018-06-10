6d526100f0b093e2bf68464191566ed4ba7616b5
Ardalan Samimi <ardalan.samimi@gmail.com>
2018-06-10 22:34:08 +0200

    Added printError method

--- Sources/SwiftArgs/SwiftArgs.swift
+++ Sources/SwiftArgs/SwiftArgs.swift
@@ -6,12 +6,17 @@ public struct SwiftArgs {
 
 	let parser: SwiftArgsParser
 
-  init(arguments: [Argument] = []) {
+	init(arguments: [Argument] = []) {
 		self.parser = SwiftArgsParser(arguments: arguments)
-  }
+	}
 
 	func parse(_ arguments: [String]? = nil) throws {
 		try self.parser.start(arguments)
+	}
+
+	func printError(_ error: Error) {
+		let error = error as? SwiftArgsError ?? error
+		print(error)
 	}
 
 }
